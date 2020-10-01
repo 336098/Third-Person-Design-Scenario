7ff96a2fe93aec562239b20bd8539282019647c7
336098 <33336196+336098@users.noreply.github.com>
2020-10-01 02:41:53 -0500

    Added Physics Materials
    Set up movement, max speed, and physics materials to change speeds

---
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10;
+
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
+
+    private new Rigidbody rigidBody;
+    private Vector2 input;
+    private new Collider collider;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+
+        collider.material = inputDirection.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicsMaterial;
+
+        //if (inputDirection.magnitude > 0)
+        //{
+        //    collider.material = movingPhysicsMaterial;
+        //}
+        //else
+        //{
+        //    collider.material = stoppingPhysicsMaterial;
+        //}
+
+        if (rigidBody.velocity.magnitude < maxSpeed)
+        {
+            rigidBody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
+        }
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+}
